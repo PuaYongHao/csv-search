10eb66ac343f2db20770484980c54e791b3df32d
Pua Yong Hao <puayonghao@outlook.com>
2024-12-18 23:52:38 +0800

    refactor: simplify the code

--- backend/src/tests/app.test.ts
+++ backend/src/tests/app.test.ts
@@ -6,14 +6,12 @@ import CSVModel from "../models/csv";
 // Mock the CSVModel
 jest.mock("../models/csv");
 
-const validCSVEntry = { postId: "1", id: "1", name: "mock_name", email: "mock_email", body: "mock_body" };
+const MOCK_CSV_DATA = { data: [{ postId: "1", id: "1", name: "mock_name", email: "mock_email", body: "mock_body" }] };
 
 describe("GET /csv", () => {
     describe("when there is data in database", () => {
         beforeAll(() => {
-            (CSVModel.findOne as jest.Mock).mockResolvedValue({
-                data: [{ ...validCSVEntry }],
-            });
+            (CSVModel.findOne as jest.Mock).mockResolvedValue(MOCK_CSV_DATA);
         });
 
         test("should respond with a 200 status code", async () => {
@@ -26,9 +24,7 @@ describe("GET /csv", () => {
         });
         test("should return the correct CSV data", async () => {
             const response = await request(app).get("/csv");
-            expect(response.body).toEqual({
-                data: [{ ...validCSVEntry }],
-            });
+            expect(response.body).toEqual(MOCK_CSV_DATA);
         });
     });
 
@@ -70,28 +66,20 @@ describe("GET /csv", () => {
 describe("POST /csv", () => {
     describe("when valid data is sent", () => {
         beforeAll(() => {
-            (CSVModel.findOneAndUpdate as jest.Mock).mockResolvedValue({
-                data: [{ ...validCSVEntry }],
-            });
+            (CSVModel.findOneAndUpdate as jest.Mock).mockResolvedValue(MOCK_CSV_DATA);
         });
 
         test("should respond with a 201 status code", async () => {
-            const response = await request(app)
-                .post("/csv")
-                .send({ data: [{ ...validCSVEntry }] });
+            const response = await request(app).post("/csv").send(MOCK_CSV_DATA);
             expect(response.statusCode).toBe(201);
         });
         test("should specify json in the content type header", async () => {
-            const response = await request(app)
-                .post("/csv")
-                .send({ data: [{ ...validCSVEntry }] });
+            const response = await request(app).post("/csv").send(MOCK_CSV_DATA);
             expect(response.headers["content-type"]).toEqual(expect.stringContaining("json"));
         });
         test("should return saved CSV data", async () => {
-            const response = await request(app)
-                .post("/csv")
-                .send({ data: [{ ...validCSVEntry }] });
-            expect(response.body).toEqual({ data: [{ ...validCSVEntry }] });
+            const response = await request(app).post("/csv").send(MOCK_CSV_DATA);
+            expect(response.body).toEqual(MOCK_CSV_DATA);
         });
     });
 
@@ -112,15 +100,11 @@ describe("POST /csv", () => {
         });
 
         test("should respond with a 500 status code", async () => {
-            const response = await request(app)
-                .post("/csv")
-                .send({ data: [{ ...validCSVEntry }] });
+            const response = await request(app).post("/csv").send(MOCK_CSV_DATA);
             expect(response.statusCode).toBe(500);
         });
         test("should return error message", async () => {
-            const response = await request(app)
-                .post("/csv")
-                .send({ data: [{ ...validCSVEntry }] });
+            const response = await request(app).post("/csv").send(MOCK_CSV_DATA);
             expect(response.body.message).toBe("Database error");
         });
     });
